aab834c92b31d2cdc01b8458f4d6128b25c4411c
duddke <sya1008@naver.com>
2022-09-19 10:24:37 +0900

    NoNet맵매니저

---
+++ Assets/SYA_Scripts/SYA_MapManager_NoNet.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class SYA_MapManager_NoNet : MonoBehaviourPun
+{
+    public List<GameObject> MAP = new List<GameObject>();
+    // Start is called before the first frame update
+    void Awake()
+    {
+        int ran = Random.Range(0, MAP.Count);
+        if (ran >= 0)
+        {
+            //PhotonNetwork.Instantiate(MAP[ran].name, Vector2.zero, Quaternion.identity);
+            GameObject map = Instantiate(MAP[ran]);
+            map.transform.position = Vector2.zero;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
